3e220d93ad8dc1b0fd00f2d2cfcd91ceb6846ace
Jakub <lemiszewski@gmx.com>
2020-04-17 00:05:39 +0200

    Added video

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -4,7 +4,8 @@ import { VideoDiv, VideoIframe, PortfolioCard } from "./styles";
 const Video = () => {
   const videoUrl = [
     "https://www.loom.com/embed/3a23fec4fdf0477cb6552f3b464b9e5c",
-    "https://www.loom.com/embed/a4b4d48d2e394bf2a31df17fe7d8493d"
+    "https://www.loom.com/embed/a4b4d48d2e394bf2a31df17fe7d8493d",
+    "https://www.loom.com/embed/e937d4ce1f004ba38f42d1c91bd799b0",
   ];
 
   return (
