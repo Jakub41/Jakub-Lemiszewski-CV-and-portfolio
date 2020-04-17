b0a71c0a0bbbfb26a18ab32afc7fb774d8fe3e2e
Jakub <lemiszewski@gmx.com>
2020-04-17 14:39:39 +0200

    Added fix for GET error on remote

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
-const express = require('express')
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
-const app = express()
-const port = process.env.PORT || 3000 // Heroku will need the PORT environment variable
+const app = express();
+const port = process.env.PORT || 3000; // Heroku will need the PORT environment variable
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, "build")));
 
-app.listen(port, () => console.log(`App is live on port ${port}!`))
+app.get("*", async (req, res) => {
+  try {
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+app.listen(port, () => console.log(`App is live on port ${port}!`));
