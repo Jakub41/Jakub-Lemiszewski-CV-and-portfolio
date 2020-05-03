9a8c2f46a5b3d48c3820305172190e8a1495e6fe
Jakub <lemiszewski@gmx.com>
2020-05-03 17:09:15 +0200

    Added capstone be

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -4,6 +4,7 @@ import { VideoDiv, VideoIframe, PortfolioCard } from "./styles";
 const Video = () => {
   const videoUrl = [
     "https://www.loom.com/embed/ec1063feb011482b8cbe10db98912c3f",
+    "https://www.loom.com/embed/425cc6ca2d524c19ab55b1924b3bf110",
     "https://www.loom.com/embed/3a23fec4fdf0477cb6552f3b464b9e5c",
     "https://www.loom.com/embed/a4b4d48d2e394bf2a31df17fe7d8493d",
     "https://www.loom.com/embed/e937d4ce1f004ba38f42d1c91bd799b0",
